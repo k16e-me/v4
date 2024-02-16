12129eae9bd5c4e3be6ebc67008c72e867636777
kb <kb@k16e.co>
2024-02-16 22:41:07 +0100

    ...

--- src/scripts/zaps.js
+++ src/scripts/zaps.js
@@ -4,9 +4,9 @@ import { _ql, _q } from './snips'
 export default function _zaps() {
     const
         tl = gsap.timeline({ ease: 'back.out', duration: 0.3 }),
-        header = _q('[data-header-main]'),
+        header = _q('[data-header]'),
         title = _q('[data-title]')
 
-    tl.from(title, { yPercent: -24, autoAlpha: 0 })
-    tl.from(header, { yPercent: 48, autoAlpha: 0 })
+    tl.from(title, { yPercent: -12, autoAlpha: 0 })
+    tl.from(header, { yPercent: 12, autoAlpha: 0 })
 }
