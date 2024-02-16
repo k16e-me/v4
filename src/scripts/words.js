775d3a508531a488ad0a9c9f5bb4d6dd8aaaeac1
kb <kb@k16e.co>
2024-02-16 20:52:33 +0100

    the push in case all those looping functs needed back

--- src/scripts/words.js
+++ src/scripts/words.js
@@ -8,48 +8,21 @@ const g = _gsap()
 export default function _words() {
     if (!_q('[data-words]')) return
 
-    /**
-        * section loads blank
-        * or gsap kicks in immediately slides one word in
-        * word slid in gets a .in class
-        * cyclicalize array, so there's always a prev el
-        * prev el to .in gets a .in class and slides in
-        * while current word gets a .out class and slides out
-        * loop
-    */
     const
         words = _ql('[data-words]'),
         delay = 3000,
         tl = gsap.timeline({ duration: 0.4, ease: 'back.in', repeat: -1 })
 
-    let
-        activeIdx = 0,
-        activeWord = words[activeIdx],
-        nextWord = null
-
     tl.from(words, {
+        opacity: 0,
+        scale: 0.8,
         yPercent: -100,
-        stagger: 2,
-        delay: 0
+        stagger: 2
     })
     tl.to(words, {
+        opacity: 0,
+        scale: 0.8,
         yPercent: 100,
         stagger: 2
     }, '<+=2')
-
-    function setNextWord() {
-        nextWord = words[getNewWordIndex(activeWord)]
-        switchWords()
-        console.log(nextWord)
-    }
-
-    function switchWords() {
-        activeWord = nextWord
-    }
-
-    function getNewWordIndex(el) {
-        let idx = words.indexOf(el) + 1
-        if (idx >= words.length) idx = 0
-        return idx
-    }
 }
