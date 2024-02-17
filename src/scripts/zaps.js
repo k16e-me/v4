c7e200580426801978c9f58a5e03e69b4cf6747a
kb <kb@k16e.co>
2024-02-17 10:52:32 +0100

    nice above below gsap entry animation

--- src/scripts/zaps.js
+++ src/scripts/zaps.js
@@ -5,8 +5,10 @@ export default function _zaps() {
     const
         tl = gsap.timeline({ ease: 'back.out', duration: 0.3 }),
         header = _q('[data-header]'),
-        title = _q('[data-title]')
+        above = _q('[data-entry="above"]'),
+        below = _ql('[data-entry="below"]')
 
-    tl.from(title, { yPercent: -12, autoAlpha: 0 })
+    tl.from(above, { yPercent: -12, autoAlpha: 0 })
     tl.from(header, { yPercent: 12, autoAlpha: 0 })
+    tl.from(below, { yPercent: 12, autoAlpha: 0 })
 }
