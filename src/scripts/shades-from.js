ef5b694cf6e5767ac76f94e8df1acadada8f8e30
kb <kb@k16e.co>
2024-02-19 19:17:30 +0100

    ...

--- src/scripts/shades-from.js
+++ src/scripts/shades-from.js
@@ -3,7 +3,7 @@ import _hue from './hue'
 export default function _shade(hex) {
     const
         { hue } = _hue(hex),
-        pastel = () => `hsl(${hue}, 97%, 92%)`,
+        pastel = () => `hsl(${hue}, 96%, 93%)`,
         dark = () => `hsl(${hue - 1}, 83%, 14%)`
 
     return {
