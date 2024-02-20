263412e09598eb0c996285789382c7c6356aba68
kb <kb@k16e.co>
2024-02-20 08:34:33 +0100

    ...

--- src/scripts/snips.js
+++ src/scripts/snips.js
@@ -4,47 +4,24 @@ const
     _q = (selector, parent) => (parent ? parent : document).querySelector(selector),
     _ql = (selector, parent) => Array.from((parent ? parent : document).querySelectorAll(selector)),
     _transition = () => new Array('transition', 'duration-200', 'ease-out'),
-    _py = () => new Array('py-16', 'sm:py-20', 'lg:py-[5.25rem]'),
-    _pb = () => new Array('pb-16', 'sm:pb-20', 'lg:pb-[5.25rem]'),
-    _pt = () => new Array('pt-16', 'sm:pt-20', 'lg:pt-[5.25rem]'),
+    _py = () => new Array('py-20', 'lg:py-[5.35rem]'),
+    _pb = () => new Array('pb-20', 'lg:pb-[5.35rem]'),
+    _pt = () => new Array('pt-20', 'lg:pt-[5.35rem]'),
     _even = n => n % 2 == 0,
     _odd = n => Math.abs(n % 2) == 1,
     _capitalize = str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase(),
-    _contrast = () => new Array('contrast-[1.2]', 'brightness-[.75]')
-
-
-const _scrollToTopOffset = (target, offset = 56) => {
-    // Credit here @ https://stackoverflow.com/questions/49820013/javascript-scrollintoview-smooth-scroll-and-offset
-    const
-        topOffset = offset,
-        elPos = Math.round(target.getBoundingClientRect().top),
-        offsetPos = (elPos + window.scrollY) - topOffset
-
-    window.scrollTo({ top: offsetPos })
-}
-
-
-const _empty = value => {
-    return (
-        value === undefined ||
-        value === null ||
-        (typeof value === "object" && Object.keys(value).length === 0) ||
-        (typeof value === "string" && value.trim().length === 0)
-    )
-}
-
-
-const _encode = data => {
-    return (
-        Object.keys(data)
-        .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-        .join('&')
-    )
-}
-
+    _contrast = () => new Array('contrast-[1.2]', 'brightness-[.75]'),
+    _empty = value => {
+        return (
+            value === undefined ||
+            value === null ||
+            (typeof value === 'object' && Object.keys(value).length === 0) ||
+            (typeof value === 'string' && value.trim().length === 0)
+        )
+    }
 
 
 // Exports
 export {
-    _body, _empty, _q, _ql, _transition, _py, _pb, _pt, _scrollToTopOffset, _even, _odd, _capitalize, _contrast, _encode
+    _body, _q, _ql, _transition, _py, _pb, _pt, _even, _odd, _capitalize, _contrast, _empty
 }
