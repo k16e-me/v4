b6b9be91fb21fea2f09059d4fe3a0d14982ebc02
kb <kb@k16e.co>
2024-02-20 18:45:12 +0100

    header work looking good

--- src/scripts/shades-from.js
+++ src/scripts/shades-from.js
@@ -4,10 +4,12 @@ export default function _shade(hex) {
     const
         { hue } = _hue(hex),
         pastel = () => `hsl(${hue}, 96%, 93%)`,
+        light = () => `hsl(${hue}, 38%, 56%)`,
         dark = () => `hsl(${hue - 1}, 83%, 14%)`
 
     return {
         pastel,
+        light,
         dark
     }
 }
