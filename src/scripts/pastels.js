b7454012b68d5db7e580516cacdaba6b9f1f559e
kb <kb@k16e.co>
2024-02-27 23:32:38 +0100

    battle testing the collection thing

---
+++ src/scripts/pastels.js
@@ -0,0 +1,10 @@
+export default function _pastels() {
+    const
+        pastels = () => [
+            '#fae8ff', '#ecfccb', '#d1fae5', '#fef3c7', '#fee2e2', '#f5f5f4', '#f1f5f9', '#e0f2fe', '#fef9c3', '#ffe4e6', '#ffedd5', '#fce7f3', '#f3e8ff', '#ede9fe', '#dcfce7', '#e0e7ff', '#dbeafe', '#ccfbf1'
+        ]
+
+    return {
+        pastels
+    }
+}
