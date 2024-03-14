e82a110ea4c0339a793cecf81d14863e76cabddd
kb <kb@k16e.co>
2024-03-14 17:24:48 +0100

    ...

--- src/scripts/backbar.js
+++ src/scripts/backbar.js
@@ -1,7 +1,21 @@
+import _scroll from './scroll'
 import { _q, _ql } from './snips'
+
 
 export default function _backbar() {
     if (!_q('[data-backbar]')) return
 
-    console.log('Back bar ran')
+    const
+        backbar = _q('[data-backbar]'),
+        scroll = Number((Math.round(backbar.nextElementSibling.getBoundingClientRect().height) / 2).toFixed()) ?? 150
+
+    _scroll(activateHeader)
+
+    function activateHeader(scrollOffset) {
+        if (scrollOffset > scroll) {
+            console.log(`You're past past the threshold!`)
+        } else {
+            console.log(`You're at the top`)
+        }
+    }
 }
