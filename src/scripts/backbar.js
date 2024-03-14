8e6a39a2e1afeeb5ce1771aa74f04c8aff225845
kb <kb@k16e.co>
2024-03-14 18:41:06 +0100

    done and dusted

--- src/scripts/backbar.js
+++ src/scripts/backbar.js
@@ -1,21 +1,24 @@
 import _scroll from './scroll'
 import { _q, _ql } from './snips'
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
 
+gsap.registerPlugin(ScrollTrigger)
 
 export default function _backbar() {
     if (!_q('[data-backbar]')) return
 
     const
         backbar = _q('[data-backbar]'),
-        scroll = Number((Math.round(backbar.nextElementSibling.getBoundingClientRect().height) / 2).toFixed()) ?? 150
-
-    _scroll(activateHeader)
-
-    function activateHeader(scrollOffset) {
-        if (scrollOffset > scroll) {
-            console.log(`You're past past the threshold!`)
-        } else {
-            console.log(`You're at the top`)
-        }
-    }
+        scroll = (backbar?.nextElementSibling?.getBoundingClientRect().height / 2) ?? 200,
+        showBackbar = gsap.from(backbar, {
+            yPercent: -100,
+            autoAlpha: 0,
+            ease: 'sine.out',
+            duration: 0.3,
+            scrollTrigger: {
+                start: scroll,
+                toggleActions: 'play none none reverse'
+            }
+        })
 }
