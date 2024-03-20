4e66d226819c7e228e00751ea3cad86c87d28be1
kb <kb@k16e.co>
2024-03-20 11:10:34 +0100

    redirecting (within astro config) possible names to dictionary

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,5 +21,11 @@ export default defineConfig({
     ],
     build: {
         format: 'preserve'
+    },
+    redirects: {
+        '/blog/ideas/possible-component-names': {
+            status: 301,
+            destination: '/blog/ideas/dictionary'
+        }
     }
 })
