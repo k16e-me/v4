d8c3632006b99a97f5c8bf8126f059a294cd8771
kb <kb@k16e.co>
2024-06-09 17:22:25 +0100

    ...

--- src/scripts/pathworx.js
+++ src/scripts/pathworx.js
@@ -4,7 +4,7 @@ export default function _paths(path) {
         isCollection = () => pathname.length === 3,
         isCollectionChild = () => pathname.length === 4,
         collection = () => pathname[1],
-        collectionParent = () => `${pathname[1]}/${pathname[2]}`
+        collectionParent = () => pathname.length === 2 ? `${pathname[1]}/${pathname[2]}` : pathname[1]
 
     return {
         isCollection, isCollectionChild, collection, collectionParent
