72cfb3c0ae144132b81081c31537707460dead9c
kb <kb@k16e.co>
2024-06-09 16:38:28 +0100

    ...

--- src/scripts/pathworx.js
+++ src/scripts/pathworx.js
@@ -1,8 +1,8 @@
 export default function _paths(path) {
     const
         pathname = path.pathname.split('/'),
-        isCollection = () => pathname.length == 3,
-        isCollectionChild = () => pathname.length == 4,
+        isCollection = () => pathname.length === 3,
+        isCollectionChild = () => pathname.length === 4,
         collection = () => pathname[1],
         collectionParent = () => `${pathname[1]}/${pathname[2]}`
 
