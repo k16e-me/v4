1c8f8472dabc5f6ea5c3dbcf3e28aa79d55c4325
kb <kb@k16e.co>
2024-06-09 22:08:43 +0100

    trailing slash woe solved

--- src/scripts/pathworx.js
+++ src/scripts/pathworx.js
@@ -1,10 +1,13 @@
+import _unslash from '@scripts/unslash'
+
 export default function _paths(path) {
+    let strippedPath = _unslash(path.pathname)
     const
-        pathname = path.pathname.split('/'),
+        pathname = strippedPath.split('/'),
         isCollection = () => pathname.length === 3,
         isCollectionChild = () => pathname.length === 4,
         collection = () => pathname[1],
-        collectionParent = () => pathname.length === 2 ? `${pathname[1]}/${pathname[2]}` : pathname[1]
+        collectionParent = () => `${pathname[1]}/${pathname[2]}`
 
     return {
         isCollection, isCollectionChild, collection, collectionParent
