7a492f7222d883664c86bd7f9ea6382b9baf4f13
kb <kb@k16e.co>
2024-06-17 15:49:50 +0100

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,7 +20,7 @@ export default defineConfig({
             // [rehypeFigure, { className: 'r-figure' }]
         ],
         remarkPlugins: [
-            [remarkFigureCaption, { captionClassName: 'r-figcaption' }]
+            remarkFigureCaption
         ]
     },
     integrations: [
