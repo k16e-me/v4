aa57cc05387eabdd0e6828fabe75a92de56d99dc
K16E, Inc <kb@k16e.co>
2024-12-15 16:55:52 +0100

    progress with timeline task tracker

--- src/scripts/components.js
+++ src/scripts/components.js
@@ -4,6 +4,8 @@ import Tiles from '@composites/Tiles.astro'
 import Richtext from '@composites/Richtext.astro'
 import Floats from '@composites/Floats.astro'
 import Collection from '@composites/Collection.astro'
+import Tasks from '@composites/Tasks.astro'
+import Heading from '@atoms/Heading.astro'
 
 export const components = {
     Title,
@@ -11,5 +13,7 @@ export const components = {
     Tiles,
     Richtext,
     Floats,
-    Collection
+    Collection,
+    Tasks,
+    Heading,
 }
