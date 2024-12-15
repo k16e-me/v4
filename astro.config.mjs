b9c7badf7f4e07cb64534660e401f8ee5b963b66
K16E, Inc <kb@k16e.co>
2024-12-15 08:16:02 +0100

    upgraded v5, attempted timelines

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,25 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import pages from 'astro-pages'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import remarkFigureCaption from '@microflash/remark-figure-caption'
 import widont from 'rehype-widont'
-// import rehypeFigure from 'rehype-figure'
 
 export default defineConfig({
     site: 'https://k16e.co',
     prefetch: {
         prefetchAll: true,
-        defaultStrategy: 'viewport',
         ignoreSlowConnection: true
     },
     markdown: {
         rehypePlugins: [
             widont,
-            // [rehypeFigure, { className: 'r-figure' }]
         ],
         remarkPlugins: [
             remarkFigureCaption
         ]
     },
     integrations: [
-        pages('routes'),
         tailwind({
             nesting: true
         }),
---
+++ src/scripts/components.js
@@ -0,0 +1,15 @@
+import Title from '@composites/Title.astro'
+import BlockText from '@composites/BlockText.astro'
+import Tiles from '@composites/Tiles.astro'
+import Richtext from '@composites/Richtext.astro'
+import Floats from '@composites/Floats.astro'
+import Collection from '@composites/Collection.astro'
+
+export const components = {
+    Title,
+    BlockText,
+    Tiles,
+    Richtext,
+    Floats,
+    Collection
+}
