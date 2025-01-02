3636a1418e83149c42776bc5fe0f665bde07dba1
K16E, Inc <kb@k16e.co>
2025-01-02 09:09:41 +0100

    now properly sorting by status and by number within a status group

--- src/scripts/sort-tasks.js
+++ src/scripts/sort-tasks.js
@@ -4,6 +4,11 @@ export function _sortTasksByStatus(tasks) {
     return tasks.sort((a, b) => {
         const statusA = statusOrder.indexOf(a.data.status)
         const statusB = statusOrder.indexOf(b.data.status)
-        return statusA - statusB
+
+        if (statusA !== statusB) return statusA - statusB
+
+        const numberA = parseInt(a.id.split('/').pop().split('-')[0])
+        const numberB = parseInt(b.id.split('/').pop().split('-')[0])
+        return numberA - numberB
     })
 }
