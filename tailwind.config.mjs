a430a15346afba4f2d29e402f67b921d766cd495
K16E, Inc <kb@k16e.co>
2025-02-05 00:00:11 +0100

    ...

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,6 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,md,mdx,ts}'],
     theme: {
         extend: {
             screens: {
@@ -12,16 +11,6 @@ export default {
             fontFamily: {
                 sans: ['Bricolage Grotesque Variable, sans-serif', ...defaultTheme.fontFamily.sans],
             },
-            fontSize: {
-                xs: ['0.895rem', '1.355rem'],
-                sm: ['0.995rem', '1.375rem'],
-                base: ['1rem', '1.555rem'],
-                md: ['1.075rem', '1.655rem'],
-                lg: ['1.125rem', '1.635rem'],
-                xl: ['1.25rem', '1.755rem'],
-                '2xl': ['1.5rem', '2.155rem'],
-                '3xl': ['1.875rem', '2.375rem'],
-            }
         }
     }
 }
